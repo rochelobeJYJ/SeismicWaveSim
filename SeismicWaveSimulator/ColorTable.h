8a05ceda26fe3b0020fda224cad312a93b7c6a64
rochelobeJYJ <ur7shadow2@gmail.com>
2026-04-18 16:02:44 +0900

    Replace C++ engine with web-based HTML5 Simulator

--- SeismicWaveSimulator/ColorTable.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Color.h>
-#include <map>
-
-namespace ColorTable {
-	enum ColorType {LAYER, P_WAVE, S_WAVE, P_REFRACTED_WAVE, S_REFRACTED_WAVE, RECEIVER, GRAPH};
-	extern const std::map<ColorType, Color::RGB> value;
-}
